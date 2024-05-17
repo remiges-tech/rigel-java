bbfa9aeb06cdf77c7c7883d80160ddcdadf0efaf
prasadn <prasad.nikam@remiges.tech>
2024-05-17 23:06:19 +0530

    refs: #19037 : Etcd key change as add the app and module and version

--- Remiges_RigelClientLib/src/main/java/com/remiges/rigel/constant/RigelConstant.java
+++ Remiges_RigelClientLib/src/main/java/com/remiges/rigel/constant/RigelConstant.java
@@ -3,5 +3,5 @@ package com.remiges.rigel.constant;
 public class RigelConstant {
 
     public static final String ETCD_SERVER_ADDRESS = "http://localhost:2379";
-    public static final String ETCD_KEY_PREFIX = "/remiges/rigel/crux/WFE/1/config/";
+    public static final String ETCD_KEY_PREFIX = "/remiges/rigel/FinanceApp/PaymentGateway/2/config";
 }
